ad6593d83ffb5e05f89ea83ec109fbd5f5dc6f88
Pushin <geiby@mail.ru>
2021-10-20 22:44:23 +0300

    hw6 start

--- app/src/main/java/com/gb/weatherapp/Extentions.kt
+++ app/src/main/java/com/gb/weatherapp/Extentions.kt
@@ -1,6 +1,8 @@
 package com.gb.weatherapp.framework
 
+import android.content.Context
 import android.view.View
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 
 fun View.showSnackBar(
@@ -12,3 +14,6 @@ fun View.showSnackBar(
 ) {
     Snackbar.make(this, text, length).setAction(actionText, action).show()
 }
+//Extention функция для тоста
+fun Context.toast(message: String) =
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
